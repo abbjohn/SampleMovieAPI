b10382cb30254cefadef9d8ec7a1d97cb3d25a69
John Abbott <abbjohn@gmail.com>
2021-03-31 16:23:10 +0100

    Initial Commit of skeleton of a sample .net Core 3.1 WebApi

---
+++ SampleMovieApi/Controllers/MetaDataController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SampleMovieApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MetaDataController : ControllerBase
+    {
+        private readonly ILogger<MetaDataController> _logger;
+
+        public MetaDataController(ILogger<MetaDataController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
---
+++ SampleMovieApi/Controllers/MoviesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SampleMovieApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly ILogger<MoviesController> _logger;
+
+        public MoviesController(ILogger<MoviesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [Route("[action]")]
+        [HttpGet]
+        public IEnumerable<string> Stats()
+        {
+            return new string[] { "value3", "value4" };
+        }
+    }
+}
